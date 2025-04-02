53fe2fe0e5f115c88a72850b81d0cd8605555ddf
Gao Sun <gao@silverhand.io>
2025-04-02 11:43:53 -0700

    release: v1.0.1

--- logto.php
+++ logto.php
@@ -6,7 +6,7 @@ if (!defined('ABSPATH'))
 /*
  * Plugin Name: Logto - User Authentication and Authorization
  * Description: Enable beautiful and secure user authentication, including passwordless, social login, single sign-on (SSO), multi-factor authentication (MFA). Generic OAuth2, OpenID Connect, SAML are also supported. Use role-based access control (RBAC) to manage user permissions.
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: Logto
  * Author URI: https://logto.io/
  * License: MPL-2.0
